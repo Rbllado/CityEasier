7eb0d51d5d25681c6e7248b223a49d23df18b479
Rafael Bosch <rbllado@outlook.com>
2019-11-22 19:11:29 +0100

    Trying to do popoulate with restaurants in city file

--- CodeProject/routes/cityRoute.js
+++ CodeProject/routes/cityRoute.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+const City = require("./../models/CitiesModel");
+// const restaurants = require("./../models/RestaurantModel");
 
 
 
 /* GET localhost3000/city/. */
+// the best way is to take from the index one the name of the city
 router.get('/barcelona', function(req, res, next) {
     // Conectar con la base de datos, y sacar valor por nombre
     // function que conecte con la base de datos, hacer un find() --> y que sea el nombre barcelona.
-    res.render("city", {city: "barcelona"});
+    
+    
+    // Esto peta por alguna cosa que esta mal ,no se exactamente que.
+    City.find({ name: 'Barcelona'})
+      .populate("restaurants")
+      
+      .then( (arrayREstaurantCollection) => {
+        console.log(arrayREstaurantCollection);
+        
+        res.render("city", {arrayREstaurantCollection});
+
+      } )
+      .catch( (err) => console.log(err));
   });
 
   /* GET localhost3000/city. */
