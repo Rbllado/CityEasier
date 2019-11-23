27babf54992993d9099c54f434950ac0e5589d39
Rafael Bosch <rbllado@outlook.com>
2019-11-23 12:58:32 +0100

    take value of the city and render the page city with categories

--- CodeProject/routes/cityRoute.js
+++ CodeProject/routes/cityRoute.js
@@ -1,53 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const City = require("./../models/CitiesModel");
 // const restaurants = require("./../models/RestaurantModel");
 
-
-
 /* GET localhost3000/city/. */
 // the best way is to take from the index one the name of the city
-router.get('/barcelona', function(req, res, next) {
-    // Conectar con la base de datos, y sacar valor por nombre
-    // function que conecte con la base de datos, hacer un find() --> y que sea el nombre barcelona.
-    
-    
-    // Esto peta por alguna cosa que esta mal ,no se exactamente que.
-    City.find({ name: 'Barcelona'})
-      .populate("restaurants")
-      
-      .then( (arrayREstaurantCollection) => {
-        console.log(arrayREstaurantCollection);
-        
-        res.render("city", {arrayREstaurantCollection});
+router.get("/barcelona", function(req, res, next) {
+  // We connect to the databse and find the name from city that we have in the body
 
-      } )
-      .catch( (err) => console.log(err));
-  });
+  City.find({ name: "Barcelona" })
+    .then(city => {
+      res.render("city", { city });
+    })
+    .catch(err => console.log(err));
 
-  /* GET localhost3000/city. */
-router.get('/london', function(req, res, next) {
-    // Conectar con la base de datos, y sacar valor por nombre
-    // function que conecte con la base de datos, hacer un find() --> y que sea el nombre london.
-    res.render("city", {city: "london"});
-  });
+  // .populate("restaurants")
 
+  // .then( (arrayREstaurantCollection) => {
+  //   console.log(arrayREstaurantCollection);
 
-  /* GET home page. */
-router.get('/naples', function(req, res, next) {
-    // Conectar con la base de datos, y sacar valor por nombre
-    // function que conecte con la base de datos, hacer un find() --> y que sea el nombre naples.
-    res.render("city", {city: "naples"});
-  });
+  //   res.render("city", {arrayREstaurantCollection});
 
+  // } )
+  // .catch( (err) => console.log(err));
+});
 
+/* GET home page. */
+router.get("/naples", function(req, res, next) {
 
-  /* GET home page. */
-router.get('/paris', function(req, res, next) {
-    // Conectar con la base de datos, y sacar valor por nombre
-    // function que conecte con la base de datos, hacer un find() --> y que sea el nombre paris.
-    res.render("city", {city: "paris"});
-  });
-  
+  // We connect to the databse and find the name from city that we have in the body
+  City.find({ name: "Naples" })
+    .then(city => {
+      res.render("city", { city });
+    })
+    .catch(err => console.log(err));
+});
 
-  module.exports = router;
+/* GET home page. */
+router.get("/paris", function(req, res, next) {
+  // We connect to the databse and find the name from city that we have in the body
+  City.find({ name: "Paris" })
+    .then(city => {
+      res.render("city", { city });
+    })
+    .catch(err => console.log(err));
+});
+
+//   /* GET localhost3000/city. */
+// router.get('/london', function(req, res, next) {
+//     // Conectar con la base de datos, y sacar valor por nombre
+//     // function que conecte con la base de datos, hacer un find() --> y que sea el nombre london.
+//     res.render("city", {city: "london"});
+//   });
+
+module.exports = router;
