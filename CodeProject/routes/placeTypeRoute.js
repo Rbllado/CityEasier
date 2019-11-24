95ec1dc9cdaf2160c5b5a6f0bd7ab42d228ad458
Rafael Bosch <rbllado@outlook.com>
2019-11-24 23:02:14 +0100

    show all restaurants in a city

--- CodeProject/routes/placeTypeRoute.js
+++ CodeProject/routes/placeTypeRoute.js
@@ -4,41 +4,25 @@ const City = require("./../models/CitiesModel");
 const Restaurant = require("./../models/RestaurantModel");
 
 /* GET localhost3000/placetype/:nombreCiudad/restaurantes. */
-// Populate is not working, we need to fix it
+
+// To work with populate I needed the models that I want to populate. In this case City and Restaurant
 router.get("/:nameCity/restaurants", function(req, res, next) {
-  City.find({ name: req.params.nameCity }, (err, city) =>{
-    
-        Restaurant.populate(city, {path: "restaurants"},(err, detailRestaurant) => {
-          // res.status(200).send(city);
-          console.log(detailRestaurant[0].restaurants[0].contact.address);
-          
-          res.render("placeType", { detailRestaurant });
-        }); 
+  // Search into the city collection in the DB, the name that we have in the url.
+  City.find({ name: req.params.nameCity }, (err, city) => {
+    // Restauran collection to do the populate, inside the collections city the field restaurants
+    Restaurant.populate(
+      city,
+      { path: "restaurants" },
+      (err, restaurantsByCity) => {
+        // res.status(200).send(city);
+        // console.log(detallRest);
 
-  } )
-  // .then(detailRestaurant => {
-  //   // console.log(detailRestaurant[0].restaurants);
-  //   res.render("placeType", { detailRestaurant });
-  // })
-  // .catch(err => console.log(err));
-
-
-
-    // .then( (city) => { console.log("Que ciudad : ", city)
-    //  })
-
-    // .populate
-    // ("Restaurant")
-
-
-    // .exec(function(err, city) {
-    //   if (err) return handleError(err);
-    //   console.log("The  ", city[0].restaurants);
-    // });
-
-
-
-
+        const arrayRestaurantInCity = restaurantsByCity[0].restaurants;
+        
+        res.render("placeType", { arrayRestaurantInCity });
+      }
+    );
+  });
 });
 
 router.get("/:nameCity/museums", function(req, res, next) {
