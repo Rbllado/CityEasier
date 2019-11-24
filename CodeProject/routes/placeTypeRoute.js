d623ec7341d6eaa7777c138de0f9727a1a57b3cb
Rafael Bosch <rbllado@outlook.com>
2019-11-24 22:08:19 +0100

    populate for restaurants done it

--- CodeProject/routes/placeTypeRoute.js
+++ CodeProject/routes/placeTypeRoute.js
@@ -1,29 +1,50 @@
 var express = require("express");
 var router = express.Router();
 const City = require("./../models/CitiesModel");
+const Restaurant = require("./../models/RestaurantModel");
 
 /* GET localhost3000/placetype/:nombreCiudad/restaurantes. */
 // Populate is not working, we need to fix it
 router.get("/:nameCity/restaurants", function(req, res, next) {
-  City.find({ name: req.params.nameCity })
+  City.find({ name: req.params.nameCity }, (err, city) =>{
+    
+        Restaurant.populate(city, {path: "restaurants"},(err, detailRestaurant) => {
+          // res.status(200).send(city);
+          console.log(detailRestaurant[0].restaurants[0].contact.address);
+          
+          res.render("placeType", { detailRestaurant });
+        }); 
+
+  } )
+  // .then(detailRestaurant => {
+  //   // console.log(detailRestaurant[0].restaurants);
+  //   res.render("placeType", { detailRestaurant });
+  // })
+  // .catch(err => console.log(err));
+
+
+
     // .then( (city) => { console.log("Que ciudad : ", city)
     //  })
-    .populate({
-      path: "city.restaurants",
-      model: "City"
-      // ("Restaurant")
-    })
-    .then(detailRestaurant => {
-      // console.log(detailRestaurant[0].restaurants);
-      res.render("placeType", { detailRestaurant });
-    })
-    .catch(err => console.log(err));
+
+    // .populate
+    // ("Restaurant")
+
+
+    // .exec(function(err, city) {
+    //   if (err) return handleError(err);
+    //   console.log("The  ", city[0].restaurants);
+    // });
+
+
+
+
 });
 
 router.get("/:nameCity/museums", function(req, res, next) {
   City.find({ name: req.params.nameCity })
     .populate("Museum")
-    .populate('City')
+    .populate("City")
     .then(detailMuseum => {
       res.render("placeType", { detailMuseum });
     })
