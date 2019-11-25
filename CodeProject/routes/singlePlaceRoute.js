b9d50e05ff08994006169660fda6f1c473eaf495
Rafael Bosch <rbllado@outlook.com>
2019-11-25 13:13:40 +0100

    single place for restaurants

--- CodeProject/routes/singlePlaceRoute.js
+++ CodeProject/routes/singlePlaceRoute.js
@@ -9,14 +9,11 @@ const rest = require("./../models/RestaurantModel");
 // Conectar con la base de datos, y sacar valor por nombre
 // function que conecte con la base de datos, --> take value from name
 
-/* GET localhost3000/placetype/. */
+/* GET localhost3000/singlePlace/. */
 router.get('/:nameRestaurant', function(req, res, next) {
 
   rest.find({name: req.params.nameRestaurant})
   .then( (restaurant) => {
-
-    
-    
     res.render("singlePlace", {restaurant});
   })
   .catch( (err) => console.log(err));
