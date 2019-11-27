bdf3f1f74546c72816f0049a7b18be9a57c50895
MichalR <mrudny@gmail.com>
2019-11-27 12:44:02 +0100

    make style layout and index

--- CodeProject/public/javascripts/main.js
+++ CodeProject/public/javascripts/main.js
@@ -1,8 +1,16 @@
-
 const buttonFav = document.querySelector(".favourite");
 const buttonEditProfile = document.querySelector(".edit-profile");
 
 var buttonDeleteFav = document.getElementsByClassName('remove-favorite');
+
+var btnMenu = document.querySelector('.side-menu-trigger');
+var sideMenu = document.querySelector('.side-menu');
+
+btnMenu.addEventListener('click', function (event) {
+    console.log('apretado');
+
+    sideMenu.classList.toggle('open');
+});
 
 
 // Add favourite element into profile page.
@@ -11,40 +19,40 @@ if (buttonFav) {
     buttonFav.addEventListener("click", (e) => {
 
         const restaurantId = (e.target.dataset.placeid);
-        
+
         axios.get(`http://localhost:3000/favourite?id=${restaurantId}`)
-        .then( (response) => {
-            buttonFav.style.background = 'red'
-            console.log('response ',response);
-            
-        })
-        .catch( (err) => console.log(err));
+            .then((response) => {
+                buttonFav.style.background = 'red'
+                console.log('response ', response);
+
+            })
+            .catch((err) => console.log(err));
     })
 }
 
 
 // Button to delete the favourite elemenst in the profile page.
 
-if (buttonDeleteFav){
-        
-        for(let i = 0;i<buttonDeleteFav.length;i++){        
-            buttonDeleteFav[i].addEventListener('click', (e) => {
-                const deleteId = (e.target.dataset.placeid);
-                
-                axios.get(`http://localhost:3000/favouriteDelete?id=${deleteId}`)
-                .then( (response) => {
+if (buttonDeleteFav) {
+
+    for (let i = 0; i < buttonDeleteFav.length; i++) {
+        buttonDeleteFav[i].addEventListener('click', (e) => {
+            const deleteId = (e.target.dataset.placeid);
+
+            axios.get(`http://localhost:3000/favouriteDelete?id=${deleteId}`)
+                .then((response) => {
                     buttonDeleteFav[i].style.background = 'blue';
                     location.reload();
-                    
+
                 })
-                .catch( (err) => console.log(err));
-            });
-        }
-        
-    };
+                .catch((err) => console.log(err));
+        });
+    }
+
+};
 
 // Modify profile edit, redirect when press the button edit.
-if(buttonEditProfile){
+if (buttonEditProfile) {
     buttonEditProfile.addEventListener("click", (e) => {
         window.location.replace("http://localhost:3000/private/edit");
     })
